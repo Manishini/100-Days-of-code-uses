4e5f5c84b094795cef6acb3b5cf5816da902c5bd
Manishini <MANISHINI.24602@stu.upes.ac.in>
2025-12-04 21:46:11 +0530

    Add files via upload

---
+++ code55.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter the upper limit (n): ");
+    scanf("%d", &n);
+
+    printf("Prime numbers up to %d are: ", n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;
+        
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
